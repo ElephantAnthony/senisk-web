9e89a7f402cd495d3aebc15939caaec34de603e5
yewon <ye7648@gmail.com>
2023-08-07 10:33:56 +0900

    introapp

--- kioskapp/urls.py
+++ kioskapp/urls.py
@@ -5,7 +5,7 @@ from kioskapp.views import KioskUi, KioskStore, KioskColor, KioskMenu, KioskDone
 app_name = "kioskapp"
 
 urlpatterns = [
-    path('create/', KioskUi.as_view(), name='ui'),
+    path('ui/', KioskUi.as_view(), name='ui'),
     path('information/', KioskStore.as_view(), name='information'),
     path('color/', KioskColor.as_view(), name='color'),
     path('menu/', KioskMenu.as_view(), name='menu'),
